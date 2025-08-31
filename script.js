0063b415686ff3331768573e3c406febd27ce3ff
Subodh <143589757+Subodh-Dhamala@users.noreply.github.com>
2025-08-31 16:43:00 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-  document.querySelector('.menu-toggle').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('show');
-  });
+const menuButton = document.querySelector(".menu-bar");
+let isToggle = false;
 
-  
+menuButton.addEventListener("click", () => {
+  const menu = document.querySelector(".menu-list");
+  if (!isToggle) {
+    menu.style.display = "block";
+    isToggle = true;
+  } else {
+    menu.style.display = "none";
+    isToggle = false;
+  }
+});
